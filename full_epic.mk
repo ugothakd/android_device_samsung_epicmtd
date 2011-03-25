11430e9da33f832191a38b09d0471a96ebfb2972
Dameon <dameon1987@gmail.com>
2011-03-25 18:08:05 -0400

    Fixed typo, thanks jspielberg

--- full_epic.mk
+++ full_epic.mk
@@ -109,7 +109,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/samsung/epic/prebuilt/apns-conf.xml:system/etc/apns-conf.xml \
     device/samsung/epic/prebuilt/gps.xml:system/etc/gps.xml \
-    device/samsung/epic/prebuilt/gps.aries.so:system/lib/hw/gps.default.so
+    device/samsung/epic/prebuilt/gps.aries.so:system/lib/hw/gps.default.so \
     device/samsung/epic/prebuilt/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
     
 # Install the features available on this device.
