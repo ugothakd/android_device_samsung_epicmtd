5cf5522664b542390e16314c6b0c3775b965c4aa
Will Keaney <keaneyw@gmail.com>
2011-05-11 20:14:59 +0000

    This ought to bring us into line with the TeamHacksung configs.
    Change-Id: Iec31102b721a734d868536451cade125114205e9

--- epic.mk
+++ epic.mk
@@ -14,97 +14,19 @@
 # limitations under the License.
 #
 
-# First we inherit other products we might need.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/small_base.mk)
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
-$(call inherit-product-if-exists, vendor/samsung/epic/epic-vendor.mk)
-$(call inherit-product, device/samsung/epic/media_a1026.mk)
+# These is the hardware-specific overlay, which points to the location
+# of hardware-specific resource overrides, typically the frameworks and
+# application settings that are stored in resourced.
+DEVICE_PACKAGE_OVERLAYS := device/samsung/epic/overlay
 
-# Property Overrides
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240 \
-    rild.libpath=/system/lib/libsec-ril40.so \
-    rild.libargs=-d[SPACE]/dev/ttyS0 \
-    wifi.interface=eth0 \
-    wifi.supplicant_scan_interval=90 \
-    ro.wifi.channels=11 \
-    ro.config.vc_call_vol_steps=15 \
-    ro.telephony.default_network=4 \
-    ro.com.google.clientidbase=android-sprint-us \
-    ro.cdma.home.operator.numeric=310120 \
-    ro.cdma.home.operator.alpha=Sprint \
-    net.cdma.pppd.authtype=require-pap \
-    net.cdma.pppd.user=user[SPACE]SprintNextel \
-    net.cdma.datalinkinterface=/dev/ttyCDMA0 \
-    net.interfaces.defaultroute=cdma \
-    net.cdma.ppp.interface=ppp0 \
-    net.connectivity.type=CDMA1 \
-    ro.csc.sales_code=SPR \
-    ril.sales_code=SPR \
-    ro.carrier=Sprint \
-    net.dns1=8.8.8.8 \
-    net.dns2=8.8.4.4 \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-meta=true \
-    media.stagefright.enable-scan=true \
-    media.stagefright.enable-http=true \
-    dalvik.vm.startheapsize=8m \
-    dalvik.vm.heapsize=32m \
-    mobiledata.interfaces=eth0,ppp0 \
-    ro.opengles.version=131072
-
-# we have enough storage space to hold precise GC data
-PRODUCT_TAGS += dalvik.gc.type-precise
-    
-# Use high-density artwork when available
-PRODUCT_LOCALES := hdpi
-
-DEVICE_PACKAGE_OVERLAYS += device/samsung/epic/overlay
-
-# Libs
-PRODUCT_PACKAGES += \
-    libSEC_OMX_Core \
-    libOMX.SEC.AVC.Decoder \
-    libOMX.SEC.M4V.Decoder \
-    libOMX.SEC.M4V.Encoder \
-    libOMX.SEC.AVC.Encoder \
-    libaudio \
-    libstagefrighthw \
-    overlay.s5pc110 \
-    copybit.s5pc110 \
-    lights.s5pc110 \
-    libs3cjpeg \
-    libcamera \
-    gps.aries
-
-# Copy product specific files
-PRODUCT_COPY_FILES += \
-    device/samsung/epic/prebuilt/media_profiles.xml:system/etc/media_profiles.xml \
+# These are the hardware-specific configuration files
+PRODUCT_COPY_FILES := \
     device/samsung/epic/prebuilt/asound.conf:system/etc/asound.conf \
-    device/samsung/epic/prebuilt/apns-conf.xml:system/etc/apns-conf.xml \
-    device/samsung/epic/prebuilt/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    device/samsung/epic/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
-    device/samsung/epic/prebuilt/vold.conf:system/etc/vold.conf \
     device/samsung/epic/prebuilt/vold.fstab:system/etc/vold.fstab \
-    device/samsung/epic/prebuilt/init.smdkc110.rc:root/init.smdkc110.rc \
-    vendor/samsung/epic/proprietary/lib/libgps.so:obj/lib/libgps.so \
-    vendor/samsung/epic/proprietary/lib/libsecgps.so:obj/lib/libsecgps.so
+    device/samsung/epic/prebuilt/vold.conf:system/etc/vold.conf \
+    device/samsung/epic/prebuilt/asound.conf:system/etc/asound.conf \
+    device/samsung/epic/prebuilt/egl.cfg:system/lib/egl/egl.cfg
 
-
-# Install the features available on this device.
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
- 
 # Keylayout and Keychars
 PRODUCT_COPY_FILES += \
      device/samsung/epic/prebuilt/keylayout/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl \
@@ -113,26 +35,138 @@ PRODUCT_COPY_FILES += \
      device/samsung/epic/prebuilt/keylayout/qt602240_ts_input.kl:system/usr/keylayout/qt602240_ts_input.kl \
      device/samsung/epic/prebuilt/keylayout/melfas-touchkey.kl:system/usr/keylayout/melfas-touchkey.kl \
      device/samsung/epic/prebuilt/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
-     device/samsung/epic/prebuilt/keychars/s3c-keypad.kcm.bin:system/usr/keychars/s3c-keypad.kcm.bin \
      device/samsung/epic/prebuilt/keychars/sec_jack.kcm.bin:system/usr/keychars/sec_jack.kcm.bin \
      device/samsung/epic/prebuilt/keychars/melfas-touchkey.kcm.bin:system/usr/keychars/melfas-touchkey.kcm.bin \
      device/samsung/epic/prebuilt/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
      device/samsung/epic/prebuilt/keychars/qt602240_ts_input.kcm.bin:system/usr/keychars/qt602240_ts_input.kcm.bin \
-     device/samsung/epic/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin  
-    
+     device/samsung/epic/prebuilt/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin
+
+# Generated kcm keymaps
+PRODUCT_PACKAGES := \
+    cypress-touchkey.kcm \
+    s3c-keypad.kcm
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# These are the OpenMAX IL configuration files
+PRODUCT_COPY_FILES += \
+    device/samsung/common/aries/sec_mm/sec_omx/sec_omx_core/secomxregistry:system/etc/secomxregistry \
+    device/samsung/epic/prebuilt/media_profiles.xml:system/etc/media_profiles.xml
+# These are the OpenMAX IL modules
+PRODUCT_PACKAGES += \
+    libSEC_OMX_Core.aries \
+    libOMX.SEC.AVC.Decoder.aries \
+    libOMX.SEC.M4V.Decoder.aries \
+    libOMX.SEC.M4V.Encoder.aries \
+    libOMX.SEC.AVC.Encoder.aries
+
+# Misc other modules
+PRODUCT_PACKAGES += \
+    lights.aries \
+    overlay.aries \
+    sensors.aries
+
+# Libs
+PRODUCT_PACKAGES += \
+    libcamera \
+    libstagefrighthw
+
+# apns config file
+PRODUCT_COPY_FILES += \
+        vendor/cyanogen/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+
+# The OpenGL ES API level that is natively supported by this device.
+# This is a 16.16 fixed point number
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.opengles.version=131072
+
+# These are the hardware-specific settings that are stored in system properties.
+# Note that the only such settings should be the ones that are too low-level to
+# be reachable from resources or other mechanisms.
+PRODUCT_PROPERTY_OVERRIDES += \
+       wifi.interface=eth0 \
+       wifi.supplicant_scan_interval=20 \
+       dalvik.vm.heapsize=32m
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.com.google.locationfeatures=1 \
+        ro.com.google.networklocation=1
+# Extended JNI checks
+# The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs
+# before they have a chance to cause problems.
+# Default=true for development builds, set by android buildsystem.
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0 \
+    dalvik.vm.checkjni=false
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# Screen density is actually considered a locale (since it is taken into account
+# the the build-time selection of resources). The product definitions including
+# this file must pay attention to the fact that the first entry in the final
+# PRODUCT_LOCALES expansion must not be a density.
+PRODUCT_LOCALES := hdpi
+
+# Property Overrides
+# PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.sf.lcd_density=240 \
+#    rild.libpath=/system/lib/libsec-ril40.so \
+#    rild.libargs=-d[SPACE]/dev/ttyS0 \
+#    ro.wifi.channels=11 \
+#    ro.config.vc_call_vol_steps=15 \
+#    ro.telephony.default_network=4 \
+#    ro.com.google.clientidbase=android-sprint-us \
+#    ro.cdma.home.operator.numeric=310120 \
+#    ro.cdma.home.operator.alpha=Sprint \
+#    net.cdma.pppd.authtype=require-pap \
+#    net.cdma.pppd.user=user[SPACE]SprintNextel \
+#    net.cdma.datalinkinterface=/dev/ttyCDMA0 \
+#    net.interfaces.defaultroute=cdma \
+#    net.cdma.ppp.interface=ppp0 \
+#    net.connectivity.type=CDMA1 \
+#    ro.csc.sales_code=SPR \
+#    ril.sales_code=SPR \
+#    ro.carrier=Sprint \
+#    net.dns1=8.8.8.8 \
+#    net.dns2=8.8.4.4 \
+#    media.stagefright.enable-player=true \
+#    media.stagefright.enable-meta=true \
+#    media.stagefright.enable-scan=true \
+#    media.stagefright.enable-http=true \
+#    dalvik.vm.startheapsize=8m \
+#    mobiledata.interfaces=eth0,ppp0 \
+
+# Copy product specific files
+PRODUCT_COPY_FILES += \
+    device/samsung/epic/prebuilt/init.smdkc110.rc:root/init.smdkc110.rc \
+    vendor/samsung/epic/proprietary/lib/libgps.so:obj/lib/libgps.so \
+    vendor/samsung/epic/proprietary/lib/libsecgps.so:obj/lib/libsecgps.so
+
 # We are using a prebuilt kernel for now, to ease building. This will be changed later.
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/epic/kernel
+    LOCAL_KERNEL := device/samsung/epic/kernel
 else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
-
-
-PRODUCT_NAME := full_epic
-PRODUCT_DEVICE := epic
-PRODUCT_MODEL := SAMSUNG-SPH-D700
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
