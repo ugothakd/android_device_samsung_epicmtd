c52ebda8849ffa756a47ceebeee42fd1e32a4e39
Decad3nce <abegovic23@gmail.com>
2011-05-16 17:11:18 -0500

    epic: Merge teamhacksung stuffs in preparation for 2.6.35 kernel. Commented things that need to be fixed related to release tools and recover. Preliminary.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,14 +3,10 @@
 # Set this up here so that BoardVendorConfig.mk can override it
 BOARD_USES_GENERIC_AUDIO := false
 
+BOARD_USES_LIBSECRIL_STUB := true
+
 # Use the non-open-source parts, if they exist
 -include vendor/samsung/epic/BoardConfigVendor.mk
-
-# Board and platform defines
-TARGET_BOARD := SPH-D700
-TARGET_BOARD_PLATFORM := s5pv210
-TARGET_BOOTLOADER_BOARD_NAME := aries
-TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
 
 # Processor optimizations
 TARGET_CPU_ABI := armeabi-v7a
@@ -21,14 +17,19 @@ TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ANDROID_ARM_LINKER := true
 
-# Set Audio related defines below.
-BOARD_USES_GENERIC_AUDIO := false
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
-# The GL userspace driver is partially broken, so we set this to get a functional UI.
-BOARD_NO_RGBX_8888 := true
+TARGET_PROVIDES_INIT := true
+TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_BOARD_PLATFORM := s5pv210
+TARGET_BOOTLOADER_BOARD_NAME := aries
+##FIXME#TARGET_RECOVERY_INITRC := device/samsung/epic/recovery.rc
 
-# We need to use the old sensors libraries, as they were changed in Gingerbread.
-TARGET_USES_OLD_LIBSENSORS_HAL := true
+# Releasetools
+TARGET_PROVIDES_RELEASETOOLS := true
+##FIXME#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/epic/releasetools/epic_ota_from_target_files
+##FIXME#TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/epic/releasetools/epic_img_from_target_files
 
 # Camera defines
 USE_CAMERA_STUB := false
@@ -36,11 +37,30 @@ ifeq ($(USE_CAMERA_STUB),false)
 BOARD_CAMERA_LIBRARIES := libcamera
 endif
 
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
 # Video Devices
 BOARD_USES_OVERLAY := true
 BOARD_V4L2_DEVICE := /dev/video1
 BOARD_CAMERA_DEVICE := /dev/video0
 BOARD_SECOND_CAMERA_DEVICE := /dev/video2
+
+# Max image/partition sizes
+BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
+BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
+BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
+BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
+# The size of a block that can be marked bad.
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# Kernel defines
+BOARD_NAND_PAGE_SIZE := 4096 -s 128
+BOARD_KERNEL_BASE := 0x02e00000
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_PREBUILT_KERNEL := device/samsung/epic/kernel
 
 # WIFI defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
@@ -53,61 +73,21 @@ WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_
 WIFI_DRIVER_MODULE_NAME := "dhd"
 BOARD_WEXT_NO_COMBO_SCAN := true
 
-# USB Tether defines
-RNDIS_DEVICE := "/sys/devices/virtual/sec/switch/tethering"
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
-# Bluetooth defines
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_BLUETOOTH := true
-BT_USE_BTL_IF := true
-BT_ALT_STACK := true
-BRCM_BTL_INCLUDE_A2DP := true
-BRCM_BT_USE_BTL_IF := true
-WITH_A2DP := true
-
-# Yamaha Sensors patch
-USE_YAMAHA_SENSORS := true
-
-# Use HGL
-BOARD_USES_HGL := true
-
-# EGL config
-BOARD_EGL_CFG := device/samsung/epic/prebuilt/egl.cfg
-
-# Kernel defines
-TARGET_PREBUILT_KERNEL := device/samsung/epic/kernel
-BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
-BOARD_KERNEL_BASE := 0x02e00000
-
-# Misc board defines
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_USES_FFORMAT := true
-BOARD_USES_BMLUTILS := true
-BOARD_RECOVERY_IGNORE_BOOTABLES := true
-TARGET_NO_RADIOIMAGE := true
-TARGET_NO_BOOTLOADER := true
-
-# The below helps to properly switch for mass storage on pre 2.6.35 kernels
-BOARD_USE_USB_MASS_STORAGE_SWITCH := true
-
-# Max image/partition sizes
-BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
-BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
-BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
-BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
-# The size of a block that can be marked bad.
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Setup the release tools. This allows us to build OTA Packages.
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/epic
-
-# ClockworkMod Defines. Define keymapping, and UMS (USB Mounting)
-BOARD_UMS_LUNFILE := "/sys/devices/platform/s3c-usbgadget/gadget/lun0/file"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/epic/recovery/recovery_ui.c
-
-BOARD_GPS_LIBRARIES := libsecgps libgps
-BOARD_USES_GPSSHIM := true
+BOARD_HAS_NO_RECOVERY_PARTITION := true
+BOARD_HAS_MTD_CACHE := true
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/mmcblk0p1
+BOARD_USES_BML_OVER_MTD := true
+BOARD_USES_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/common/aries/shbootimg.mk
+TARGET_NO_RECOVERY := true
+TARGET_RECOVERY_PRE_COMMAND="echo 1 > /cache/.startrecovery; sync;"
 
 # Include aries specific stuff
 -include device/samsung/common/aries.mk
