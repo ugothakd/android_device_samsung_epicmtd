9da261635de23f767dc01178e1dbbfddbba2e606
jt1134 <jt1134@gmail.com>
2011-10-07 20:30:48 -0500

    BoardConfig and prebuilt cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,8 @@ BOARD_USES_LIBSECRIL_STUB := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 ARCH_ARM_HAVE_TLS_REGISTER := true
 ANDROID_ARM_LINKER := true
 
@@ -21,18 +21,20 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 TARGET_BOARD_PLATFORM := s5pv210
+TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
+TARGET_BOOTLOADER_BOARD_NAME := s5pc110
 
 # Provide our own libaudio
 TARGET_PROVIDES_LIBAUDIO := true
-
-# Samsung still doesn't love its ugly stepchild
-BOARD_NO_RGBX_8888 := true
 
 # Camera defines
 USE_CAMERA_STUB := false
 ifeq ($(USE_CAMERA_STUB),false)
 BOARD_CAMERA_LIBRARIES := libcamera
 endif
+
+# OpenGL stuff
+BOARD_EGL_CFG := device/samsung/epic/egl.cfg
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -72,20 +74,16 @@ WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_
 WIFI_DRIVER_MODULE_NAME := "dhd"
 BOARD_WEXT_NO_COMBO_SCAN := true
 
-# Because fuck you, that's why
-BOARD_USES_PREBUILT_LIBNETUTILS := true
-
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_RECOVERY_PARTITION := true
-BOARD_HAS_MTD_CACHE := true
 BOARD_HAS_SDCARD_INTERNAL := true
 BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/mmcblk0p1
-BOARD_USES_CUSTOM_BOOTIMG := true
 TARGET_NO_RECOVERY := true
 
-# Include aries specific stuff
--include device/samsung/aries-common/aries.mk
+# Override cyanogen squisher to customize our update zip package
+TARGET_CUSTOM_RELEASETOOL := ./device/samsung/epic/releasetools/squisher
+
