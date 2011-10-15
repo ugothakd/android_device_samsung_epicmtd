be056e041e7874927f8f8ef7cd11223eb52bbbc1
Decad3nce <abegovic23@gmail.com>
2011-10-15 00:34:02 -0500

    Fix routing for MODE_RINGTONE in AudioHardware so that incoming calls actually ring -- lolwut

--- libaudio/AudioHardware.cpp
+++ libaudio/AudioHardware.cpp
@@ -669,17 +669,17 @@ const char *AudioHardware::getOutputRouteFromDevice(uint32_t device)
     case AudioSystem::DEVICE_OUT_EARPIECE:
         return "RCV";
     case AudioSystem::DEVICE_OUT_SPEAKER:
-        if (mMode == AudioSystem::MODE_RINGTONE) return "RING_SPK";
-        else return "SPK";
+        if (mMode == AudioSystem::MODE_RINGTONE) return "SPK";
+        else return "RING_SPK";
     case AudioSystem::DEVICE_OUT_WIRED_HEADPHONE:
-        if (mMode == AudioSystem::MODE_RINGTONE) return "RING_NO_MIC";
-        else return "HP_NO_MIC";
+        if (mMode == AudioSystem::MODE_RINGTONE) return "HP_NO_MIC";
+        else return "RING_NO_MIC";
     case AudioSystem::DEVICE_OUT_WIRED_HEADSET:
-        if (mMode == AudioSystem::MODE_RINGTONE) return "RING_HP";
+        if (mMode == AudioSystem::MODE_RINGTONE) return "HP";
         else return "HP";
     case (AudioSystem::DEVICE_OUT_SPEAKER|AudioSystem::DEVICE_OUT_WIRED_HEADPHONE):
     case (AudioSystem::DEVICE_OUT_SPEAKER|AudioSystem::DEVICE_OUT_WIRED_HEADSET):
-        if (mMode == AudioSystem::MODE_RINGTONE) return "RING_SPK_HP";
+        if (mMode == AudioSystem::MODE_RINGTONE) return "HP";
         else return "SPK_HP";
     case AudioSystem::DEVICE_OUT_BLUETOOTH_SCO:
     case AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_HEADSET:
