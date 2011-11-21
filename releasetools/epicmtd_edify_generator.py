3abd2fb8139df62ae755e51c2042348c06463cad
noobnl <Shareefalis@gmail.com>
2011-11-21 17:19:11 -0500

    add kexec to boot.img when the first time to convert block device to MTD

--- releasetools/epicmtd_edify_generator.py
+++ releasetools/epicmtd_edify_generator.py
@@ -48,7 +48,10 @@ class EdifyGenerator(edify_generator.EdifyGenerator):
       self.script.append(
             ('package_extract_file("bml_over_mtd.sh", "/tmp/bml_over_mtd.sh");\n'
              'set_perm(0, 0, 0777, "/tmp/bml_over_mtd.sh");'))
-
+      self.script.append(
+            ('package_extract_file("kexec", "/tmp/kexec");\n'
+             'set_perm(0, 0, 0777, "/tmp/kexec");'))
+        
       self.script.append('package_extract_file("boot.img", "/tmp/boot.img");')
       self.script.append('assert(run_program("/tmp/updater.sh") == 0);')
 
