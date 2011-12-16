5284d515226690b1a6bcc1598f1ef9f23940c55b
noobnl <Shareefalis@gmail.com>
2011-12-16 05:07:57 -0500

    fix ordering of mk file to better suite compatibly with build system

--- Android.mk
+++ Android.mk
@@ -14,6 +14,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
 include device/samsung/aries-common/libs3cjpeg/Android.mk
 include device/samsung/aries-common/AriesParts/Android.mk
+include $(call all-makefiles-under,$(LOCAL_PATH))
